b614779b6c29fb2802b27ed13cc37eae2adeb629
Rose <26861054+Roachbones@users.noreply.github.com>
2019-07-05 23:58:28 -0400

    taught roomba to patrol

--- roomba.gd
+++ roomba.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-export(int) var speed = 512
-export(float) var rotation_speed = 0.01
+export(int) var speed = 2
+export(float) var rotation_speed = 0.05
 export(float) var adjustment_angle = PI * 0.7
 
 var target_rotation = 0
@@ -10,10 +10,13 @@ var collision: KinematicCollision2D
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
-	if rotation <= target_rotation:
-		collision = move_and_collide(Vector2(1, 0).rotated(rotation))
+func _physics_process(_delta):
+	rotation = fposmod(rotation, TAU)
+	if target_rotation <= 0: #if done rotating for now
+		collision = move_and_collide(Vector2(1, 0).rotated(rotation) * speed)
 		if collision:
 			target_rotation += adjustment_angle
 	else:
+		print(rotation)
 		rotation += rotation_speed
+		target_rotation -= rotation_speed
