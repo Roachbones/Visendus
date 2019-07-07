42c1374d8f6623f06f8d115217fc2cbd25337bd9
Rose <26861054+Roachbones@users.noreply.github.com>
2019-07-07 17:38:53 -0400

    added dialog trigger

--- HUD.gd
+++ HUD.gd
@@ -3,16 +3,19 @@ extends CanvasLayer
 export var progress_path: NodePath
 export var system_log_path: NodePath
 export var node_preview_path: NodePath
+export var transmission_log_path: NodePath
 
 var progress: TextureProgress
 var system_log: RichTextLabel
 var node_preview: TextureRect
+var transmission_log: RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	progress = get_node(progress_path)
 	system_log = get_node(system_log_path)
 	node_preview = get_node(node_preview_path)
+	transmission_log = get_node(transmission_log_path)
 
 func _on_Ides_scan_progress_changed(scan_progress):
 	progress.value = scan_progress
@@ -22,3 +25,7 @@ func _on_Ides_logged_bbcode(bbcode):
 
 func _on_Ides_scanned_node_changed(scanned_node: Node2D):
 	node_preview.set_texture(scanned_node.get_node("Sprite").texture) #probably unreliable
+
+func append_transmission(bbcode):
+	print(bbcode)
+	assert transmission_log.append_bbcode(bbcode) == OK
