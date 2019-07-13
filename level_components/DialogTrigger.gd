3bcefade9f8c193adaa810289322858b26bb52a6
Rose <26861054+Roachbones@users.noreply.github.com>
2019-07-12 20:55:38 -0400

    made DialogTrigger more flexible

--- level_components/DialogTrigger.gd
+++ level_components/DialogTrigger.gd
@@ -4,12 +4,16 @@ export(String, MULTILINE) var bbcode_dialog = "Placeholder dialog."
 const BRUCE_COLOR = "f5a142"
 onready var colored_bbcode_dialog = "[color=#" + BRUCE_COLOR + "]" + bbcode_dialog + "[/color]\n"
 var triggered = false
+var hud
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if get_tree().get_nodes_in_group("hud"):
+		hud = get_tree().get_nodes_in_group("hud")[0]
+	else:
+		assert false
 
 func _on_DialogTrigger_body_entered(body):
-	if not triggered and body == get_parent().get_node("Ides"):
-		get_parent().get_node("HUD").append_transmission(colored_bbcode_dialog)
+	if not triggered and body.is_in_group("ides") and hud:
+		hud.append_transmission(colored_bbcode_dialog)
 		triggered = true
