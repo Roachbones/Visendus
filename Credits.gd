add37a875e196a02c027ab67142935fcb46580f8
Rose <26861054+Roachbones@users.noreply.github.com>
2019-07-16 23:00:54 -0400

    fixed LossLabyrinth loading zone

--- Credits.gd
+++ Credits.gd
@@ -1,36 +1,47 @@
 extends RichTextLabel
 
 const credits_text = """
-Game made by Vivian and Dero
+Game made by Vivian and Dero for [i]My First Game Jam: Summer 2019[/i]
 
 Sources used:
-
-Freesound.org
-	[i]Alert[/i] - flowerdove2168
-	[i]Drone dark emptiness[/i] - ceich93
-	[i]Low resonant dark tone[/i] - Robinhood76	
-	[i]Low pitch drone plus[/i] - benbonecan
-	[i]Low pitch drone 1-3[/i] - benbonecan	
-	[i]Pencil sharpener with crank[/i] - roivasugo
-	[i]Sorpresa 2[/i] - poderarcano
-	[i]Whispers[/i] - thanvannispen
-
-Dig.ccmixter.org
-	[i]Excessive exposure[/i] - acclivity
-	[i]Eyes Closed[/i] - mwic
+	Freesound.org
+		[i]Acoustic guitar[/i] - frankyboomer
+		[i]Alert[/i] - flowerdove2168
+		[i]Cavernous drone[/i] - glueissobar
+		[i]Drone dark emptiness[/i] - ceich93
+		[i]Dark ambience[/i] - patricklieberkind
+		[i]London underground train interior[/i] - inspectorj
+		[i]Low resonant dark tone[/i] - Robinhood76	
+		[i]Low pitch drone plus[/i] - benbonecan
+		[i]Low pitch drone 1-3[/i] - benbonecan	
+		[i]Pencil sharpener with crank[/i] - roivasugo
+		[i]Sorpresa 2[/i] - poderarcano
+		[i]Whispers[/i] - thanvannispen
+		[i]Windchime tolling[/i] - inspectorj
+	Dig.ccmixter.org
+		[i]Excessive exposure[/i] - acclivity
+		[i]Eyes Closed[/i] - mwic
 
 Where can you find us??? RIGHT HERE
-
 	Vivian
 		[url=https://rose.systems/]https://rose.systems/[/url]
 	Dero
 		Twitter: [url=https://twitter.com/inhumanrobot]@inhumanrobot[/url]
 		Instagram: [url=https://www.instagram.com/inhumanrobot/]@inhumanrobot[/url]
+
+Thank you for playing our game.
 """
+var credits_text_lines = credits_text.split("\n")
 
-
+var sum_delta = 0.0
+const seconds_per_line = 0.2
 func _ready():
 	pass
+func _process(delta):
+	sum_delta += delta
+	#visible_characters = int(sum_delta * characters_per_second)
+	if sum_delta > seconds_per_line:
+		sum_delta = sum_delta - seconds_per_line
 
 func _on_Credits_meta_clicked(meta):
 	var err = OS.shell_open(meta)
