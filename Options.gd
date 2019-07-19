ae2a250998938ac28b4776e5694e5424dfa281cc
Rose <26861054+Roachbones@users.noreply.github.com>
2019-07-19 01:23:29 -0400

    moved speedrun results

--- Options.gd
+++ Options.gd
@@ -3,7 +3,7 @@ extends Node
 # warning-ignore:unused_class_variable
 var seer_rays_enabled = false
 # warning-ignore:unused_class_variable
-var speedrun_mode_enabled = true
+var speedrun_mode_enabled = false
 # warning-ignore:unused_class_variable
 var use_scene_transition = false
 # warning-ignore:unused_class_variable
